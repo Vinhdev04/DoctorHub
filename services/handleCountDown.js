0749675ca7568f97c146f850110edcbb346df502
Phạm Công Vinh <pcv.fed@gmail.com>
2025-03-31 00:36:32 +0700

    Update code Page [Shop]

---
+++ services/handleCountDown.js
@@ -0,0 +1,118 @@
+document.addEventListener("DOMContentLoaded", function () {
+  new Splide(".splide-shop", {
+    type: "loop",
+    autoplay: true,
+    interval: 4000,
+    speed: 1000,
+    perPage: 1,
+    focus: "center",
+    pagination: false,
+    arrows: true,
+    breakpoints: {
+      768: { perPage: 1 },
+    },
+  }).mount();
+});
+
+const startCountdown = () => {
+  const endTime = new Date();
+  // đặt mốc hết ngày
+  endTime.setHours(23, 59, 59);
+
+  const updateCountdown = () => {
+    const now = new Date();
+
+    // khoảng cách thời gian còn lại
+    const distance = endTime - now;
+
+    // kiểm tra điều kiện
+    if (distance < 0) {
+      document.getElementById("hours").textContent = "00";
+      document.getElementById("minutes").textContent = "00";
+      document.getElementById("seconds").textContent = "00";
+      return;
+    }
+
+    // tính toán thời gian còn lại theo miliseconds  không vượt quá 24 giờ
+    const hours = Math.floor((distance / (1000 * 60 * 60)) % 24);
+    const minutes = Math.floor((distance / (1000 * 60)) % 60);
+    const seconds = Math.floor((distance / 1000) % 60);
+
+    // gán dữ liệu vào DOM - Nếu chuỗi chỉ có 1 chữ số, thêm số 0 vào trước để đủ 2 chữ số.
+    document.getElementById("hours").textContent = String(hours).padStart(
+      2,
+      "0"
+    );
+    document.getElementById("minutes").textContent = String(minutes).padStart(
+      2,
+      "0"
+    );
+    document.getElementById("seconds").textContent = String(seconds).padStart(
+      2,
+      "0"
+    );
+  };
+  // chạy lần đâu tiên
+  updateCountdown();
+  setInterval(updateCountdown, 1000);
+};
+startCountdown();
+document.addEventListener("DOMContentLoaded", function () {
+  new Splide(".splide-medical", {
+    type: "loop",
+    perPage: 5, // Hiển thị 4 sản phẩm trên mỗi slide
+    perMove: 1,
+    gap: "20px", // Khoảng cách giữa các item
+    padding: "1rem",
+    // autoplay: true, badge-discount
+    interval: 3000, // Chuyển slide mỗi 3 giây
+    pagination: false,
+    arrows: true,
+    breakpoints: {
+      1200: { perPage: 4 }, // Màn hình nhỏ hơn 1200px hiển thị 3 sản phẩm
+      992: { perPage: 2 }, // Màn hình nhỏ hơn 992px hiển thị 2 sản phẩm
+      768: { perPage: 2 }, // Màn hình nhỏ hơn 768px hiển thị 1 sản phẩm
+      576: { perPage: 1 }, // Màn hình nhỏ  hơn 576px hiển thị 1 sản phẩm
+    },
+  }).mount();
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  new Splide(".splide-deals", {
+    type: "loop",
+    perPage: 5, // Hiển thị 4 sản phẩm trên mỗi slide
+    perMove: 1,
+    gap: "20px", // Khoảng cách giữa các item
+    padding: "1rem",
+    // autoplay: true, badge-discount
+    interval: 3000, // Chuyển slide mỗi 3 giây
+    pagination: false,
+    arrows: true,
+    breakpoints: {
+      1200: { perPage: 4 }, // Màn hình nhỏ hơn 1200px hiển thị 3 sản phẩm
+      992: { perPage: 2 }, // Màn hình nhỏ hơn 992px hiển thị 2 sản phẩm
+      768: { perPage: 2 }, // Màn hình nhỏ hơn 768px hiển thị 1 sản phẩm
+      576: { perPage: 1 }, // Màn hình nhỏ  hơn 576px hiển thị 1 sản phẩm
+    },
+  }).mount();
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  new Splide(".splide-family", {
+    type: "loop",
+    perPage: 5, // Hiển thị 4 sản phẩm trên mỗi slide
+    perMove: 1,
+    gap: "20px", // Khoảng cách giữa các item
+    padding: "1rem",
+    // autoplay: true, badge-discount
+    interval: 3000, // Chuyển slide mỗi 3 giây
+    pagination: false,
+    arrows: true,
+    breakpoints: {
+      1200: { perPage: 4 }, // Màn hình nhỏ hơn 1200px hiển thị 3 sản phẩm
+      992: { perPage: 2 }, // Màn hình nhỏ hơn 992px hiển thị 2 sản phẩm
+      768: { perPage: 2 }, // Màn hình nhỏ hơn 768px hiển thị 1 sản phẩm
+      576: { perPage: 1 }, // Màn hình nhỏ  hơn 576px hiển thị 1 sản phẩm
+    },
+  }).mount();
+});
