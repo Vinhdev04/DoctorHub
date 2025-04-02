01901952e8e9b38fa4649399e6e7fa8543618602
Phạm Công Vinh <pcv.fed@gmail.com>
2025-04-03 01:36:24 +0700

    Update code Page Shop and Update code data for database - medicines

--- services/handleCountDown.js
+++ services/handleCountDown.js
@@ -116,3 +116,23 @@ document.addEventListener("DOMContentLoaded", function () {
     },
   }).mount();
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  new Splide(".splide-newMedicines", {
+    type: "loop",
+    perPage: 5,
+    perMove: 1,
+    gap: "20px",
+    padding: "1rem",
+    autoplay: true,
+    interval: 3000,
+    pagination: false,
+    arrows: true,
+    breakpoints: {
+      1200: { perPage: 4 },
+      992: { perPage: 2 },
+      768: { perPage: 2 },
+      576: { perPage: 1 },
+    },
+  }).mount();
+});
